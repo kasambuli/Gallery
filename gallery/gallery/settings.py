7db55485b43ba3ba01e7a8e7117a32ee6a08d45c
kasambuli <cynthiakasambuli@gmail.com>
2018-05-07 18:52:25 +0300

    hroku dep

--- gallery/gallery/settings.py
+++ gallery/gallery/settings.py
@@ -65,6 +65,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.media',
+                'whitenoise.middleware.WhiteNoiseMiddleware'
             ],
         },
     },
@@ -126,3 +127,6 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+import django_heroku
+django_heroku.settings(locals())
