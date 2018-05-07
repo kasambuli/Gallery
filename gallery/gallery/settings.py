f54401e101217889158b3e49c70caa106e6cdebf
kasambuli <cynthiakasambuli@gmail.com>
2018-05-07 17:23:20 +0300

    add media

--- gallery/gallery/settings.py
+++ gallery/gallery/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'gallery.urls'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -121,5 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
 MEDIA_URL = '/media/'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
